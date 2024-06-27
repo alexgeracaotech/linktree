3bda31c4ff30db4c85c069e1d6624298b5fa3800
Alex Bessa <alexbessa.dev@gmail.com>
2024-06-27 15:17:06 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleMode() {
+    const body = document.body
+    body.classList.toggle("light")
+    const img = document.querySelector("#profile img")
+    if (body.classList.contains("light")) {
+        img.setAttribute("src", "./assets/profile-light.png")
+    } else {
+        img.setAttribute("src", "/assets/profile-dark.png")
+    }
+}
